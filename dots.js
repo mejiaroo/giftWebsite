fbca0dc23ec3c9b162777d3538c75903b2e6eed8
Oscar <oscarr.mejiia@gmail.com>
2025-09-13 23:27:31 -0400

    dragonfruit update

--- dots.js
+++ dots.js
@@ -1,13 +1,9 @@
-const box = document.getElementById("password-overlay");
+const box = document.querySelector("#oval-banner .dots");
 for (let i = 0; i < 200; i++) {
   const dot = document.createElement("div");
   dot.classList.add("dot");
 
   const size = 10;
   dot.style.width = dot.style.height = size + "px";
-
-  // keep dot inside the box (subtract its size from 100%)
-  dot.style.left = Math.random() * (100 - (size / box.offsetWidth * 100)) + "%";
-  dot.style.top = Math.random() * (100 - (size / box.offsetHeight * 100)) + "%";
   box.appendChild(dot);
 }
