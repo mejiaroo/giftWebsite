7639224c7f7716dc8d24f1135220b9f586278c6b
Oscar <oscarr.mejiia@gmail.com>
2025-09-13 21:26:16 -0400

    Made slight aesthetic changes + added audio

---
+++ README.md
@@ -0,0 +1,3 @@
+# A simple website made for someone very special
+
+Sorry for the long block of text that you are about to read but Ive been meaning to make a proper response to your little notion blog you made. I wanted to write down every thought ive had about you and about our relationship since we started talking again because I make you overthink a lot and I never wanted to hurt you.
--- dots.js
+++ dots.js
@@ -3,11 +3,11 @@ for (let i = 0; i < 200; i++) {
   const dot = document.createElement("div");
   dot.classList.add("dot");
 
-  const size = 10; // dot size (5px–15px)
+  const size = 10;
   dot.style.width = dot.style.height = size + "px";
 
   // keep dot inside the box (subtract its size from 100%)
   dot.style.left = Math.random() * (100 - (size / box.offsetWidth * 100)) + "%";
-  dot.style.top  = Math.random() * (100 - (size / box.offsetHeight * 100)) + "%";
+  dot.style.top = Math.random() * (100 - (size / box.offsetHeight * 100)) + "%";
   box.appendChild(dot);
 }
